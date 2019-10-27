c331f91a4ff4acf22e5b1f3a5d5eaddaa35bc227
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-27 05:47:54 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_reader :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    self.backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+
+end
