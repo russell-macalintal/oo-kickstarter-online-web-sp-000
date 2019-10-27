e651c75e8f35a2fa04dde0cdf1e36807ec0411d5
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-27 05:47:45 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
 
   def add_backer(backer)
     self.backers << backer
-    # backer.back_project(self)
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 
 end
